0e9b692cc55c567a4f52a108dac611b96e92d64f
Joe Bell <joeaintexas@gmail.com>
2013-12-14 15:55:05 -0600

    Initial Commit

---
+++ flatbutton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  flatbutton
+//
+//  Created by Joseph Bell on 12/14/13.
+//  Copyright (c) 2013 iAchieved.it LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
